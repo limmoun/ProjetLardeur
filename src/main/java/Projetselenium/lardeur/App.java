f741cda5c46cd44782c0f6116aa02b59681c6476
lyn <linim@outlook.fr>
2021-02-26 16:57:38 +0100

    init project

---
+++ src/main/java/Projetselenium/lardeur/App.java
@@ -0,0 +1,13 @@
+package Projetselenium.lardeur;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/testing/TestLardeurTest.java
@@ -0,0 +1,72 @@
+package testing;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Objects;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+
+public class TestLardeurTest {
+WebDriver driver;
+
+@Before
+
+public void setup() {
+	//URL e=this.getClass().getClassLoader().getResource("driver.chromedriver.exe");
+	//File file = new File(((URL)Objects.requireNonNull(e)).getFile());
+			
+	System.setProperty("webdriver.chrome.driver","src/main/resources/driver/chromedriver.exe");
+	driver = new ChromeDriver();
+}
+//@After
+public void teardown() {
+	driver.quit();
+}
+
+ @Test
+public void TestLardeur() throws Exception {
+
+	driver.get("https://fr.wowhead.com");
+	
+	//passer les cookies
+	driver.findElement(By.xpath("//*[@id=\"onetrust-accept-btn-handler\"]")).click();
+	Thread.sleep(10000);
+	driver.findElement(By.xpath("//div[@class='notifications-dialog-buttons']/button[text()='Refuser']")).click();
+	
+	//saisir une recherche et la valider
+	WebElement element = driver.findElement(By.xpath("//*[@name='q']"));
+	element.click();
+	element.clear();
+	element.sendKeys("Lardeur");
+	driver.findElement(By.xpath("//a[@class='header-search-button fa fa-search']")).click();
+	
+	assertTrue(driver.findElement(By.xpath("//div[@class='text']/h2[contains(text(),'Premiers Résultats pour: ')]")).isDisplayed());
+	System.out.print("Premiers Résultats pour: Lardeur");
+	
+	//cliquer sur la tete de mort "lardeur"
+	driver.findElement(By.xpath("//a[@class='top-results-result-cover'][@href='/npc=448/lardeur']")).click();
+	
+	//cliquer sur un butin
+	
+
+}
+ 
+ public void ecrireChampTxt (WebElement e, String s) {
+		e.clear();
+		e.sendKeys(s);
+
+}
+}
