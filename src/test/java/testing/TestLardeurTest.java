fcfb55b0bcb62f8a0c9eb270f42de9a11b77197d
lyn <linim@outlook.fr>
2021-02-26 17:29:49 +0100

    ajout browser

--- src/test/java/testing/TestLardeurTest.java
+++ src/test/java/testing/TestLardeurTest.java
@@ -32,8 +32,8 @@ public void setup() {
 	System.setProperty("webdriver.chrome.driver","src/main/resources/driver/chromedriver.exe");
 	driver = new ChromeDriver();
 	}
-	else {
-		System.setProperty("webdriver.geckodriver.driver","src/main/resources/driver/geckodriver.exe");
+	else if (BROWSER.equalsIgnoreCase("gecko"))	{
+			System.setProperty("webdriver.gecko.driver","src/main/resources/driver/geckodriver.exe");
 	}
 	}
 
